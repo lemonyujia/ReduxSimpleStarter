989ead390798ea9be1f10fc03657fd8c7a652773
Yujia Zhai <zhai.yujia627@gmail.com>
2018-09-09 12:08:48 -0500

    add weather list

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,21 @@
+import axios from 'axios';
+
 const API_KEY = '5e8dadbc627dd8f5becd6c3c2f242bba';
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+// const ROOT_URL = 'https://samples.openweathermap.org/data/2.5/forecast?appid='' + API_KEY;
+
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+// action creator
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  console.log('Request:', request);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request // we are returning the promise as the payload (important)
+  };
+}
